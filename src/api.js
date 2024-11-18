fec7074d216a22a627c203b0876d5bfbc85fd607
17sonu <97702823+17sonu@users.noreply.github.com>
2024-11-18 20:35:51 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://cors-backend-login.vercel.app/api', // Use the server's base URL
-});
-
-export default api;
